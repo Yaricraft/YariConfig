69cde57a6d1379924108db7ea75a54bb563356d7
yariplus <tafike@gmail.com>
2014-04-23 17:21:10 -0400

    Changed naming for base and working directories and allowed naming each seperately in options rathar than guessing their locations.

--- update_patches.py
+++ update_patches.py
@@ -27,43 +27,64 @@ def cleanDirs(path):
         os.rmdir(path)
         
 def main():
-    print "Creating patches"
+    print 
+    print "    ============================="
+    print "              YariConfig"
+    print "    ============================="
+    print "   Minecraft configuration utility."
+    print ""
     
-    strConfigDir = os.path.dirname(os.path.abspath(__file__))
+    # Get the path we are running the script in.
+    strPathCurrent = os.path.dirname(os.path.abspath(__file__))
+    
+    # Collect options
     parser = OptionParser()
-    
-    parser.add_option('-p', '--pack', action='store', dest='optPack', help='Pack to use.', default='scpb')
-    parser.add_option('-c', '--config', action='store', dest='optConfig', help='Name of configs to compare to.', default='yari')
-    
+    parser.add_option('-b', '--base', action='store', dest='optBase', help='Path to base onfigs.', default='scpb/original')
+    parser.add_option('-w', '--work', action='store', dest='optWork', help='Path to work onfigs.', default='scpb/yari')
     options, _ = parser.parse_args()
     
-    optPack = os.path.abspath(options.optPack)
-    print "Using option "+optPack
-    optConfig = os.path.abspath(options.optConfig)
-    print "Using option "+optConfig
-
-    diff = os.path.normpath(os.path.join(optPack, optConfig+'_patches'))
-    print "Patches going to "+diff
-    base = os.path.normpath(os.path.join(optPack, 'original'))
-    print "Using base configs at "+base
-    work = os.path.normpath(os.path.join(optPack, optConfig))
-    print "Using edited configs at "+work
+    # Make sure the directories are valid.
+    if options.optBase == '':
+        strPathBase = os.path.abspath('base')
+        print "No base config path specified, attempting to use "+strPathBase
+    else:
+        strPathBase = os.path.abspath(options.optBase)
+    strNormBase = os.path.normpath(strPathBase)
+    if os.path.isdir(strNormBase):
+        print "Using base configs at "+strNormBase
+    else:
+        print "Base config directory was not found at "+strNormBase
+        return
     
-    for path, _, filelist in os.walk(work, followlinks=True):
-        for cur_file in fnmatch.filter(filelist, '*.cfg'):
-            file_base = os.path.normpath(os.path.join(base, path[len(work)+1:], cur_file)).replace(os.path.sep, '/')
-            file_work = os.path.normpath(os.path.join(work, path[len(work)+1:], cur_file)).replace(os.path.sep, '/')
+    if options.optWork == '':
+        strPathWork = os.path.abspath('work')
+        print "No work config path specified, attempting to use "+strPathWork
+    else:
+        strPathWork = os.path.abspath(options.optWork)
+    strNormWork = os.path.normpath(strPathWork)
+    if os.path.isdir(strNormWork):
+        print "Using work configs at "+strNormWork
+    else:
+        print "Work config directory was not found at "+strNormWork
+        return
+    
+    strNormPatches = os.path.normpath(strPathWork.join('_patches'))
+    
+    for path, _, filelist in os.walk(strNormWork, followlinks=True):
+        for cur_file in fnmatch.filter(filelist, '*'):
+            file_base = os.path.normpath(os.path.join(strNormBase, path[len(strNormWork)+1:], cur_file)).replace(os.path.sep, '/')
+            file_work = os.path.normpath(os.path.join(strNormWork, path[len(strNormWork)+1:], cur_file)).replace(os.path.sep, '/')
             
             fromlines = open(file_base, 'U').readlines()
             tolines = open(file_work, 'U').readlines()
             
-            patch = ''.join(difflib.unified_diff(fromlines, tolines, '../' + file_base[len(strPackDir)+1:], '../' + file_work[len(strPackDir)+1:], '', '', n=3))
-            patch_dir = os.path.join(diff, path[len(work)+1:])
+            patch = ''.join(difflib.unified_diff(fromlines, tolines, '../' + file_base[len(strPathCurrent)+1:], '../' + file_work[len(strPathCurrent)+1:], '', '', n=3))
+            patch_dir = os.path.join(strNormPatches, path[len(strNormWork)+1:])
             patch_file = os.path.join(patch_dir, cur_file + '.patch')
             print('%s', patch_dir)
             
             if len(patch) > 0:
-                print patch_file[len(diff)+1:]
+                print patch_file[len(strNormPatches)+1:]
                 patch = patch.replace('\r\n', '\n')
                 
                 if not os.path.exists(patch_dir):
@@ -72,11 +93,11 @@ def main():
                     fh.write(patch)
             else:
                 if os.path.isfile(patch_file):
-                    print("Deleting empty patch: %s"%(patch_file))
+                    print "Deleting empty patch: "+patch_file
                     os.remove(patch_file)
                     
 
-    cleanDirs(diff)
+    cleanDirs(strNormPatches)
     
 if __name__ == '__main__':
     main()
